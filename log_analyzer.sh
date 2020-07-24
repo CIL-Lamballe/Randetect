bd2e5283f9d57f5eaa0238e5d1457ec446b8acc0
Antoine Barthelemy <abarthel@student.42.fr>
2020-07-24 15:45:13 +0200

    Init (DB + script)
    Adding DB and script to work on sql queries.

---
+++ log_analyzer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## Samba Log Database
+# Path
+SLDPATH='/var/log/synolog/'
+# Name
+SLDNAME='.SMBXFERDB'
+
+
+## Database format (line 1: Element name, line 2: Tuple default value) 
+#         ______________________________________________________________________________________________________________________
+# line 1 | id (int)    | time (int) | ip (text) | username (text) | cmd (text) | filesize (int) | filename (text) | isdir (int) |
+#        |-------------|------------|-----------|-----------------|------------|----------------|-----------------|-------------|
+# line 2 | primary key |    NULL    |   NULL    |     NULL        |    NULL    |     NULL       |      NULL       |     0       |
+#        |_____________|____________|___________|_________________|____________|________________|_________________|_____________|
+
+
+##
+#sqlite3 ${SLDPATH}${SLDNAME} "SELECT username FROM logs WHERE username"
+sqlite3 ${SLDPATH}${SLDNAME} "SELECT * FROM logs WHERE username LIKE '%pgik%'"
