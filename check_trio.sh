60d49fd0c0a937b37c7dce7b031dda727d11934b
Antoine Barthelemy <abarthel@student.42.fr>
2020-08-03 06:00:06 -0400

    feat(norm)

--- check_trio.sh
+++ check_trio.sh
@@ -9,61 +9,42 @@ ymin=3
 range=2000
 
 QUERY=`sqlite3 ${SLDPATH}${SLDNAME} "
-SELECT
-	D.ip
+SELECT D.ip
 FROM
 	(
-		SELECT
-			A.ip, A.username, A.filename,
+		SELECT	A.ip, A.username, A.filename,
 			B.filesize as wrotefilesize, A.cmd,
 			A.time as createtime, B.cmd,
 			B.time as writetime
 		FROM
 			(
-				SELECT
-						*
-				FROM
-					logs
-				WHERE
-					id > (
-						SELECT
-							MAX(id) - $range
-						FROM
-							logs
-						WHERE
-							isdir = 0
+				SELECT	*
+				FROM	logs
+				WHERE	id > (
+						SELECT MAX(id) - $range
+						FROM	logs
+						WHERE	isdir = 0
 					)
-	     		) A,
+			) A,
 			(
-				SELECT
-					*
-				FROM
-					logs
-				WHERE
-					id > (
-						SELECT
-							MAX(id) - $range
-						FROM
-							logs
-						WHERE
-							isdir = 0
+				SELECT	*
+				FROM	logs
+				WHERE	id > (
+						SELECT	MAX(id) - $range
+						FROM	logs
+						WHERE	isdir = 0
 					)
 	     		) B
-		WHERE
-			A.filename = B.filename
+		WHERE	A.filename = B.filename
 			AND A.cmd = 'create' AND B.cmd = 'write'
 			AND createtime <= writetime AND (writetime - createtime) <= $xmin
 	) CWp,
 	(
-		SELECT
-			*
-		FROM
-			logs
-		WHERE
-			isdir = 0 AND cmd = 'delete'
+		SELECT	*
+		FROM	logs
+		WHERE	isdir = 0 AND cmd = 'delete'
 	) D
-WHERE
-	CWp.writetime <= D.time
+WHERE	CWp.writetime <= D.time
 	AND (D.time - CWp.writetime) <= $ymin
 	AND D.filesize <= CWp.wrotefilesize
 ;"`
