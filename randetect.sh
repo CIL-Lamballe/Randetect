5e88d46d732d04639ce9e015f32fab1565dd417d
Antoine Barthelemy <abarthel@student.42.fr>
2020-08-03 08:44:51 -0400

    archi(randetect): structure on the go

--- randetect.sh
+++ randetect.sh
@@ -14,8 +14,8 @@
 # Error Log: Any errors or output associated with the script can be found in ?(not yet specified)
 #
 
+
 # Globals
-IFS=$'\n'
 #SLDPATH='/var/log/synolog/'
 SLDPATH='/home/antoine/SynologyNAS_RansomwareAnalyzer/'
 SLDNAME='.SMBXFERDB'
@@ -23,48 +23,51 @@ XMIN=1
 YMIN=3
 RANGE=2000
 BAN_LIMIT=50
-BLACKLIST=()
-COUNTER=()
-QUERY=`sqlite3 ${SLDPATH}${SLDNAME} "
-SELECT D.ip
-FROM
-	(
-		SELECT	A.ip, A.username, A.filename,
-			B.filesize as wrotefilesize, A.cmd,
-			A.time as createtime, B.cmd,
-			B.time as writetime
-		FROM
-			(
-				SELECT	*
-				FROM	logs
-				WHERE	id > (
-						SELECT MAX(id) - $RANGE
-						FROM	logs
-						WHERE	isdir = 0
-					)
-			) A,
-			(
-				SELECT	*
-				FROM	logs
-				WHERE	id > (
-						SELECT	MAX(id) - $RANGE
-						FROM	logs
-						WHERE	isdir = 0
-					)
-	     		) B
-		WHERE	A.filename = B.filename
-			AND A.cmd = 'create' AND B.cmd = 'write'
-			AND createtime <= writetime AND (writetime - createtime) <= $XMIN
-	) CWp,
-	(
-		SELECT	*
-		FROM	logs
-		WHERE	isdir = 0 AND cmd = 'delete'
-	) D
-WHERE	CWp.writetime <= D.time
-	AND (D.time - CWp.writetime) <= $YMIN
-	AND D.filesize <= CWp.wrotefilesize
-;"`
+
+
+function synology_log_query() {
+	IFS=$'\n'
+	QUERY=`sqlite3 ${SLDPATH}${SLDNAME} "
+	SELECT D.ip
+	FROM
+		(
+			SELECT	A.ip, A.username, A.filename,
+				B.filesize as wrotefilesize, A.cmd,
+				A.time as createtime, B.cmd,
+				B.time as writetime
+			FROM
+				(
+					SELECT	*
+					FROM	logs
+					WHERE	id > (
+							SELECT MAX(id) - $RANGE
+							FROM	logs
+							WHERE	isdir = 0
+						)
+				) A,
+				(
+					SELECT	*
+					FROM	logs
+					WHERE	id > (
+							SELECT	MAX(id) - $RANGE
+							FROM	logs
+							WHERE	isdir = 0
+						)
+		     		) B
+			WHERE	A.filename = B.filename
+				AND A.cmd = 'create' AND B.cmd = 'write'
+				AND createtime <= writetime AND (writetime - createtime) <= $XMIN
+		) CWp,
+		(
+			SELECT	*
+			FROM	logs
+			WHERE	isdir = 0 AND cmd = 'delete'
+		) D
+	WHERE	CWp.writetime <= D.time
+		AND (D.time - CWp.writetime) <= $YMIN
+		AND D.filesize <= CWp.wrotefilesize
+	;"`
+}
 
 
 function add_to_blacklist() {
@@ -74,6 +77,8 @@ function add_to_blacklist() {
 
 
 function parse_ip_from_query() {
+	BLACKLIST=()
+	COUNTER=()
 	local index=0
 	if [[ "${BLACKLIST[@]}" =~ "$1" ]];
 	then
@@ -96,6 +101,7 @@ function parse_ip_from_query() {
 
 
 function main() {
+	synology_log_query
 	for ip in ${QUERY}
 	do
 		parse_ip_from_query $ip
