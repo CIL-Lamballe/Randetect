7952f9feb1b9038181ce81f917295d927a47344b
Antoine Barthelemy <abarthel@student.42.fr>
2020-08-14 16:58:11 +0200

    feat(struct): adding user struct
    Init rust

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 use std::{thread, time};
 
+const MAX_LOG: u16 = 2_000; // Maximum log parsed - LIFO.
+const TIME: u64 = 2_000; // Loop dealy in milliseconds.
+
+struct User {
+    ip: String,
+    username: String,
+    filename: String,
+    attempt: i32,
+}
+
+fn hello(h: u16) -> u8 {
+    for i in 1..4 {
+        println!("Hello, world! {}", h - i);
+    }
+    5
+}
+
 fn main() {
-    let duration = time::Duration::from_millis(2000);
+    let duration = time::Duration::from_millis(TIME);
     loop {
-        println!("Hello, world!");
+        hello(MAX_LOG);
         thread::sleep(duration);
     }
 }
