b61b271d79fa1d652b3f5bd65bb0c387b8b4b368
Antoine Barthelemy <abarthel@student.42.fr>
2020-08-18 10:23:02 +0200

    feat(user): going to add objects

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,23 @@ use std::{thread, time};
 
 const MAX_LOG: u16 = 2_000; // Maximum log parsed - LIFO.
 const TIME: u64 = 2_000; // Loop dealy in milliseconds.
+const DBPATH: &str = "/var/log/synolog/"; // Path to Synology logs.
+const DB: &str = ".SMBXFERDB"; // Database containing file logs.
+
+enum ActivityType {
+    Suspicious(i32),     // Containing nb of files manipulated.
+    Misbehaving(String), // Contaning name of directory been moved.
+    Normal,              // Normal user activity.
+}
 
 struct User {
     ip: String,
     username: String,
-    filename: String,
-    attempt: i32,
+    kind: ActivityType,
 }
 
 fn main() {
-    let duration = time::Duration::from_millis(2000);
+    let duration = time::Duration::from_millis(TIME);
     loop {
         println!("Hello, world!");
         thread::sleep(duration);
