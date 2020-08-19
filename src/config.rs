b7e701db8ba27b079d88c79b5c2849aa373935d1
Antoine Barthelemy <abarthel@student.42.fr>
2020-08-19 11:15:19 +0200

    feat(mod): adding mod to give initial architecture to prog

--- src/config.rs
+++ src/config.rs
@@ -2,14 +2,20 @@
 ** Configuration file with all constants used in Randetect
 */
 
-/* Maximum log parsed - LIFO. */
-pub const MAX_LOG: u16 = 2_000;
+/* Maximum log parsed starting from last entry */
+pub const MAX_LOG: u16 = 2_500;
 
-/* Loop dealy in milliseconds. */
+/* Loop dealy in milliseconds */
 pub const TIME: u64 = 2_000;
 
-/* Path to Synology logs. */
+/* Path to Synology logs database */
 pub const DBPATH: &str = "/var/log/synolog/";
-
-/* Database containing file logs. */
 pub const DB: &str = ".SMBXFERDB";
+
+/* Randetect error log files */
+pub const LOGDIR: &str = "/var/log/randetect/";
+pub const ERRLOG: &str = "error.log";
+pub const OUTLOG: &str = "output.log";
+
+/* Maximum suspicious action limit */
+pub const BAN_LIMIT: u16 = 50;
