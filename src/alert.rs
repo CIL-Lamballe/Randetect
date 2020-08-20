c92fc431b06df6fb9552f273212c577d73b61ed3
Antoine Barthelemy <abarthel@student.42.fr>
2020-08-20 11:10:42 +0200

    feat(file): open file using error method

--- src/alert.rs
+++ src/alert.rs
@@ -1,22 +1,13 @@
 use std::fs::File;
+use std::io;
 
 pub mod sms {
     pub fn send() {}
 }
 
 pub mod email {
-    pub fn send() {
-        let f = File::open("email.txt");
-
-        let f = match f {
-            Ok(file) => file,
-            Err(error) => match error.kind() {
-                ErrorKind::NotFound => match File::create("email.txt") {
-                    Ok(fc) => fc,
-                    Err(e) => panic!("Problem creating the file: {:?}", e),
-                },
-                other_error => panic!("Problem opening the file: {:?}", other_error),
-            },
-        };
+    pub fn send() -> Result<std::fs::File, std::io::Error> {
+        let mut f = std::fs::File::open("email.txt")?;
+        Ok(f)
     }
 }
