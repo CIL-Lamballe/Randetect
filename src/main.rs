b44c57e041eb47066c6444d2ca0b015424e8b5bd
Antoine Barthelemy <abarthel@student.42.fr>
2020-08-20 14:53:26 +0200

    feat(database): implementing query analysis

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,37 @@
+use rusqlite::{Connection, Result, NO_PARAMS};
 use std::{thread, time};
-
-extern crate rusqlite;
-
-use rusqlite::{Connection, Result};
-use rusqlite::NO_PARAMS;
 
 mod alert;
 mod config;
 mod database;
 
-enum ActivityType {
-    Suspicious(i32),     // Containing nb of files manipulated.
-    Misbehaving(String), // Contaning name of directory been moved.
-    Normal,              // Normal user activity.
-}
-
-enum Ip {
-    V4(String),
-    V6(String),
-}
-
-struct User {
-    username: String,
-    ip: Ip,
-    kind: ActivityType,
-}
+//enum ActivityType {
+//    Suspicious(i32),     // Containing nb of files manipulated.
+//    Misbehaving(String), // Contaning name of directory been moved.
+//    Normal,              // Normal user activity.
+//}
+//
+//enum Ip {
+//    V4(String),
+//    V6(String),
+//}
+//
+//struct User {
+//    username: String,
+//    ip: Ip,
+//    kind: ActivityType,
+//}
 
 fn main() {
+    let db = format!("{}{}", config::DBPATH, config::DBNAME);
 
     let duration = time::Duration::from_millis(config::TIME);
 
     loop {
-        println!("Hello, world!");
+        println!("Loop");
+        database::huge_delete(&db);
         thread::sleep(duration);
-        database::dir_move();
-        alert::sms::send();
+        //        database::dir_move();
+        //        alert::sms::send();
     }
 }
