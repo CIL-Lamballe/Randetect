bf90fe34b416e4c78a2bb513d54d24f18f8fcc88
Antoine Barthelemy <abarthel@student.42.fr>
2020-08-20 09:51:12 +0200

    feat(email): error handling in email

--- src/alert.rs
+++ src/alert.rs
@@ -1,7 +1,22 @@
+use std::fs::File;
+
 pub mod sms {
     pub fn send() {}
 }
 
 pub mod email {
-    pub fn send() {}
+    pub fn send() {
+        let f = File::open("email.txt");
+
+        let f = match f {
+            Ok(file) => file,
+            Err(error) => match error.kind() {
+                ErrorKind::NotFound => match File::create("email.txt") {
+                    Ok(fc) => fc,
+                    Err(e) => panic!("Problem creating the file: {:?}", e),
+                },
+                other_error => panic!("Problem opening the file: {:?}", other_error),
+            },
+        };
+    }
 }
