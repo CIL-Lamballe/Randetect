40f297f5ceba36fa5b49bedd823ffb7345eee2f1
Antoine Barthelemy <abarthel@student.42.fr>
2020-08-25 17:02:46 +0200

    feat(hashtable): adding hashtable for user activity

--- src/parse.rs
+++ src/parse.rs
@@ -9,17 +9,17 @@ const BAN_LIMIT: u16 = 50;
 #[derive(Debug)]
 enum Behavior {
     Suspicious(i32),     // Containing nb of files manipulated.
-    Misbehaving(String), // Contaning name of directory been moved.
-    Normal,              // Normal user activity.
+        Misbehaving(String), // Contaning name of directory been moved.
+        Normal,              // Normal user activity.
 }
 
 #[derive(Debug)]
 pub struct UserInfo {
     ip: String,
-//    ip: Vec<String>,
-//    kind: ActivityType,
+    //    ip: Vec<String>,
+    //    kind: ActivityType,
     //  kind: Vec<ActivityType>,
-  //  count: i32,
+    count: i32,
 }
 
 /// Accounting of action in order to determine user behavior(Normal, Suspicious, Misbehaving)
@@ -27,5 +27,17 @@ pub fn log(
     entry: Vec<Log>,
     users: &mut HashMap<String, UserInfo>
 ) {
-
+    for el in entry {
+        if !users.contains_key("username") {
+            users.insert(
+                el.get_username(),
+                UserInfo {
+                    ip: el.get_ip(),
+                    count: 0,
+                }
+            );
+        } else {
+            println!("exist {:?}", users);
+        }
+    }
 }
