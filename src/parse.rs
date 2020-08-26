8b7e89edaf41f6e07a56bf2026b1a78afd5d763c
Antoine Barthelemy <abarthel@student.42.fr>
2020-08-26 13:40:34 +0200

    feat(parse): adding user count in log analysis, making clssification possible

--- src/parse.rs
+++ src/parse.rs
@@ -9,13 +9,12 @@ const BAN_LIMIT: u16 = 50;
 #[derive(Debug)]
 enum Behavior {
     Suspicious(i32),     // Containing nb of files manipulated.
-        Misbehaving(String), // Contaning name of directory been moved.
-        Normal,              // Normal user activity.
+    Misbehaving(String), // Contaning name of directory been moved.
+    Normal,              // Normal user activity.
 }
 
 #[derive(Debug)]
 pub struct UserInfo {
-    //ip: String,
     ip: Vec<String>,
     //    kind: ActivityType,
     //  kind: Vec<ActivityType>,
@@ -23,36 +22,33 @@ pub struct UserInfo {
 }
 
 impl UserInfo {
-   fn increment(&mut self) {
-    self.count += 1;
-   }
-
-   fn push_ip(&mut self, new: String) {
-        if !self.ip.contains(&new) {
-            self.ip.push(new);
+    fn new(ip: String) -> UserInfo {
+        UserInfo {
+            ip: {
+                let mut n = Vec::new();
+                n.push(ip);
+                n
+            },
+            count: 0,
         }
-   }
+    }
+
+    fn update(&mut self, newip: String) {
+        self.count += 1;
+        if !self.ip.contains(&newip) {
+            self.ip.push(newip);
+        }
+    }
 }
 
 /// Accounting of action in order to determine user behavior(Normal, Suspicious, Misbehaving)
-pub fn log(
-    entry: Vec<Log>,
-    users: &mut HashMap<String, UserInfo>
-) {
+pub fn log(entry: Vec<Log>, users: &mut HashMap<String, UserInfo>) {
     for el in entry {
         let uname = el.get_username();
         if !users.contains_key(&uname) {
-            users.insert(
-                uname,
-                UserInfo {
-                    ip: { let mut n = Vec::new(); n.push(el.get_ip()); n },
-                    count: 0,
-                }
-            );
+            users.insert(uname, UserInfo::new(el.get_ip()));
         } else {
-            users.get_mut(&uname).unwrap().increment();
-            let new_ip = el.get_ip();
-            users.get_mut(&uname).unwrap().push_ip(new_ip);
+            users.get_mut(&uname).unwrap().update(el.get_ip());
         }
     }
 }
