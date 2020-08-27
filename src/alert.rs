9fd7263bd86a8cf8be36e6a35d5affef0e3e6a0c
Antoine Barthelemy <abarthel@student.42.fr>
2020-08-27 14:45:51 +0200

    feat(email): email can be sent

--- src/alert.rs
+++ src/alert.rs
@@ -5,40 +5,46 @@ pub mod sms {
 }
 
 pub mod email {
+    use std::error::Error;
+    use std::fs::File;
+    use std::io::prelude::*;
+    use std::path::Path;
+    use std::process::Command;
 
-    fn create(subject: &str, text: &str){
-
-        let mail = format!("Subject: {}\n{}", subject, text);
+    fn create(subject: &str, text: &str) {
+        let mail = format!("Subject: {}\n{}\n", subject, text);
         //println!("{}", mail);
 
-        let path = std::path::Path::new("email.txt");
+        let path = Path::new("email.txt");
         let mut file = match std::fs::File::create(&path) {
-            Err(why) => panic!("Failed to create email: {}", std::error::Error::description(&why)),
+            Err(why) => panic!("Failed to create email: {}", Error::description(&why)),
             Ok(file) => file,
         };
-  //      match file.write_all(header_body.as_bytes()) {
-  //          Err(why) => panic!("Could not write to email.txt: {}", std::error::Error::description(&why)),
-  //          Ok(_) => (),
-  //      }
+        match file.write_all(mail.as_bytes()) {
+            Err(why) => panic!("Could not write to email.txt: {}", Error::description(&why)),
+            Ok(_) => (),
+        }
     }
 
     pub fn send() {
-
         let to = "a.barthleemy@cil-lamballe.com";
 
-        create("Suspicious user", "A user have been using it badly it has been banned");
+        create(
+            "Suspicious user",
+            "A user have been using it badly it has been banned",
+        );
 
         let ssmtp = "ssmtp ".to_string() + to + " < email.txt";
 
-        let output = std::process::Command::new("sh")
+        let output = Command::new("sh")
             .arg("-c")
             .arg(ssmtp)
             .output()
-            .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
+            .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
 
-        // println!("status: {}", output.status);
-        // println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-        // println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-
+        // Debug
+        //  println!("status: {}", output.status);
+        //  println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+        //  println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
     }
 }
