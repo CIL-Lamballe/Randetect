8edf3a0f01ca5ee984ecbddebcc6cfbc33a0d45d
Antoine Barthelemy <abarthel@student.42.fr>
2020-08-28 10:27:51 +0200

    feat(env): get env var for lftp

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,39 @@ mod alert;
 mod parse;
 mod query;
 
+struct Cdtl {
+    user: String,
+    pwd: String,
+    sys: String,
+    folder: String,
+    smsusr: String,
+}
+
 /// Loop delay in milliseconds
 const TIME: u64 = 2_000;
 
+/// Get environment variable for lftp use
+fn getenv(var: &str) -> String {
+    match env::var(var) {
+        Ok(val) => val,
+        Err(e) => panic!("{} : {}", var, e),
+    }
+}
+
+fn env_variables() -> Cdtl {
+    let crdtl = getenv("CRDTL");
+    Cdtl {
+        user: crdtl[..9].to_string(),
+        pwd: crdtl[10..17].to_string(),
+        sys: getenv("TARGETSYS"),
+        folder: getenv("FOLDER"),
+        smsusr: getenv("SMSUSR"),
+    }
+}
+
 fn main() {
 
-    let key = "HOME";
-    match env::var(key) {
-    Ok(val) => println!("{}: {:?}", key, val),
-    Err(e) => println!("couldn't interpret {}: {}", key, e),
-    }
-
+    let var: Cdtl = env_variables();
 
     let duration = time::Duration::from_millis(TIME);
 
