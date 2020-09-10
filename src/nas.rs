58a95a7b68f58fe6a20a816e3d79ff3343e69456
Antoine Barthelemy <abarthel@student.42.fr>
2020-09-10 16:21:14 +0200

    feat(ban): new unfailed ban system

--- src/nas.rs
+++ src/nas.rs
@@ -1,6 +1,7 @@
 use crate::parse::UserInfo;
 use serde_json::Value;
-use std::{process::Command, thread, time::Duration};
+use std::process::Command;
+//use std::{process::Command, thread, time::Duration};
 
 pub fn cmd_exec(cmd: &str) -> (String, String, String) {
     println!("{}", cmd);
@@ -47,31 +48,33 @@ fn close_request() -> String {
 
 pub fn ban(info: &UserInfo) {
     for ip in info.get_ips().iter() {
-        // Format ban request
-        let cmd = ban_profile(&ip);
-        cmd_exec(&cmd);
-        thread::sleep(Duration::from_millis(800));
+        for _retry in 0..2 {
+            // Format ban request
+            let cmd = ban_profile(&ip);
+            cmd_exec(&cmd);
+            // thread::sleep(Duration::from_millis(400));
 
-        // Apply new profile and request task_id
-        let cmd = apply_profile();
-        let (_status, stdout, _stderr) = cmd_exec(&cmd);
-        thread::sleep(Duration::from_millis(800));
-        let v: Value = serde_json::from_str(&stdout).unwrap();
-        let v: Value = serde_json::from_str(&v["data"].to_string()).unwrap();
-        let v = v["task_id"].to_string();
+            // Apply new profile and request task_id
+            let cmd = apply_profile();
+            let (_status, stdout, _stderr) = cmd_exec(&cmd);
+            //  thread::sleep(Duration::from_millis(400));
+            let v: Value = serde_json::from_str(&stdout).unwrap();
+            let v: Value = serde_json::from_str(&v["data"].to_string()).unwrap();
+            let v = v["task_id"].to_string();
 
-        // Update the profile using task_id
-        let cmd = update_profile(&v);
-        cmd_exec(&cmd);
-        thread::sleep(Duration::from_millis(800));
+            // Update the profile using task_id
+            let cmd = update_profile(&v);
+            cmd_exec(&cmd);
+            //  thread::sleep(Duration::from_millis(400));
 
-        // Finalize the request
-        let cmd = close_request();
-        cmd_exec(&cmd);
-        thread::sleep(Duration::from_millis(800));
+            // Finalize the request
+            let cmd = close_request();
+            cmd_exec(&cmd);
+            //  thread::sleep(Duration::from_millis(400));
 
-        // Restart Samba to kick off user
-        cmd_exec("/sbin/restart smbd");
+            // Restart Samba to kick off user
+            cmd_exec("/sbin/restart smbd");
+        }
     }
 }
 
