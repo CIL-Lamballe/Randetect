0fb520ba2dd9723a23d53f18c6b9fa5bd38e52cf
Antoine Barthelemy <abarthel@student.42.fr>
2020-09-11 10:52:47 +0200

    intal scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+cross build --target=aarch64-unknown-linux-musl --release
+cp target/aarch64-unknown-linux-musl/release/randetect randetect_aarch64-musl
 
+cross build --target=x86_64-unknown-linux-musl --release
+cp target/x86_64-unknown-linux-musl/release/randetect randetect_x86_64-musl
