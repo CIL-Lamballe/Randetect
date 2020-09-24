fa2fc3f6acffc03b39930d89b924d327dff21cf9
Antoine Barthelemy <abarthel@student.42.fr>
2020-09-24 11:30:52 +0200

    update(build): adding verbose

--- build.sh
+++ build.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 arch=('aarch64' 'x86_64')
 printf "Check code base:\n"
-if ! (cargo fmt && cargo clippy --all-targets --all-features -- -D clippy::pedantic)
+if (cargo fmt && cargo clippy --all-targets --all-features -- -D clippy::pedantic)
 then
 	tput bold
+	tput setaf 46
+	printf "OK!\n"
+	tput init
+
+else
+	tput bold
 	tput setaf 196
-	printf "Code is not clean, Randetect will not compile.\nPlease check the code!\n"
+	printf "FAILED!\n Code is not clean, Randetect will not compile.\nPlease check the code!\n"
 	tput init
 	exit 1
 fi
 for a in ${arch[@]}
 do
-	printf "Compiling for => "; tput setaf 93; tput bold printf "${a}"; tput init; printf "\n";
+	printf "Compiling for => "
+	tput bold
+	tput setaf 93
+	printf "$a\n"
+	tput init
 	if (cross build --target=${a}-unknown-linux-musl --release &&
 		cp target/${a}-unknown-linux-musl/release/randetect randetect_${a}-musl)
 	then
